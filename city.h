fb6e8167da576a29977ce5c5d1d91170afaae215
Ran <davidwebster@live.cn>
2013-02-19 14:27:35 -0500

    4

--- city.h
+++ city.h
@@ -2,13 +2,17 @@
 #define CITY_H
 
 #include <QString>
+#include <QList>
 
 class City
 {
 private:
     int x, y;
     QString name;
+    QList<City> conn;
+    enum{VISITED,ELIMINATED,UNVISITED} state;
 public:
+    City();
     City(QString n, int xco, int yco);
     void setX(int value);
     void setY(int value);
@@ -16,6 +20,9 @@ public:
     int getX();
     int getY();
     QString getName();
+    void addConn(City &city);
+    int nConn();
+    const City & showConn(int i);
 };
 
 #endif // CITY_H
